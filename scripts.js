0da040f16ee6788a0208132e114ca792e99d9cb8
khoabui9 <khoabui.vn@gmail.com>
2018-10-25 15:57:20 +0300

    update

--- scripts.js
+++ scripts.js
@@ -58,7 +58,7 @@ var about = [{
 }, {
     text1: "Contact me",
     text2: "khoa.buigia0212@gmail.com",
-}, ]
+},]
 
 const container = document.querySelector('.container'),
     header = createHeader(),
@@ -731,6 +731,41 @@ function render() {
         about.map((el, idx) => {
             interactive(idx)
         })
+        // Loadbar Animation
+        $(".loadbar").animate({
+            width: width + "%"
+        }, time);
+
+        // Percentage Increment Animation
+        var PercentageID = $("#precent"),
+            start = 0,
+            end = 100,
+            durataion = time;
+        animateValue(PercentageID, start, end, durataion);
+
+        function animateValue(id, start, end, duration) {
+
+            var range = end - start,
+                current = start,
+                increment = end > start ? 1 : -1,
+                stepTime = Math.abs(Math.floor(duration / range)),
+                obj = $(id);
+
+            var timer = setInterval(function () {
+                current += increment;
+                $(obj).text(current + "%");
+                //obj.innerHTML = current;
+                if (current == end) {
+                    clearInterval(timer);
+                }
+            }, stepTime);
+        }
+
+        // Fading Out Loadbar on Finised
+        setTimeout(function () {
+            $('.preloader-wrap').fadeOut(500);
+        }, time);
+
     })
 
     init();
@@ -812,38 +847,3 @@ function interactive(i) {
     inner.onmouseleave = onMouseLeaveHandler;
     inner.onmousemove = onMouseMoveHandler;
 };
-
-// Loadbar Animation
-$(".loadbar").animate({
-    width: width + "%"
-}, time);
-
-// Percentage Increment Animation
-var PercentageID = $("#precent"),
-    start = 0,
-    end = 100,
-    durataion = time;
-animateValue(PercentageID, start, end, durataion);
-
-function animateValue(id, start, end, duration) {
-
-    var range = end - start,
-        current = start,
-        increment = end > start ? 1 : -1,
-        stepTime = Math.abs(Math.floor(duration / range)),
-        obj = $(id);
-
-    var timer = setInterval(function () {
-        current += increment;
-        $(obj).text(current + "%");
-        //obj.innerHTML = current;
-        if (current == end) {
-            clearInterval(timer);
-        }
-    }, stepTime);
-}
-
-// Fading Out Loadbar on Finised
-setTimeout(function () {
-    $('.preloader-wrap').fadeOut(500);
-}, time);
